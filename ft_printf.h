22f2e47dd2279026b323c39ccc96609315890f23
Yaroslav <salvaincorp@gmail.com>
2018-02-06 19:13:43 +0200

    Working

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,23 @@
 /*   By: ypikul <ypikul@student.unit.ua>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 01:07:42 by ypikul            #+#    #+#             */
-/*   Updated: 2018/02/05 18:33:50 by ypikul           ###   ########.fr       */
+/*   Updated: 2018/02/06 13:59:44 by ypikul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# define BUFF_SIZE 64
+
 typedef struct	s_arg
 {
-	int		minus_sign_flag;
-	int		zero_flag;
-	int		plus_flag;
+	int		zero_pad;
+	int		sign_pad;
+	int		left_pad;
+
+	char	buf[BUFF_SIZE];
+	size_t	size;
 }				t_arg;
 
 int		ft_printf(const char *format, ...);
--- ft_putbuf.c
+++ ft_putbuf.c
@@ -1,40 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putbuf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ypikul <ypikul@student.unit.ua>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/14 00:03:07 by ypikul            #+#    #+#             */
-/*   Updated: 2018/02/06 14:16:43 by ypikul           ###   ########.fr       */
+/*   Created: 2018/02/06 12:41:15 by ypikul            #+#    #+#             */
+/*   Updated: 2018/02/06 18:30:39 by ypikul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
+#include <unistd.h>
 #include "ft_printf.h"
-#include "linft.h"
 
-static int	ft_vprintf(const char *format, va_list *arg, t_arg *spec, s)
+void	ft_putbuf(const char c, t_arg spec, const int forced_print_flag)
 {
-	
-	while (*format)
+	if (forced_print_flag && spec->size > 0)
 	{
-		
+		write(STDOUT_FILENO, &spec->buf, spec->size);
+		spec->size = 0;
 	}
-}
-
-int			ft_printf(const char *format, ...)
-{
-	va_list	arg;
-	t_arg	spec;
-	size_t	done;
-
-	if (format == NULL || BUFF_SIZE < 1)
-		return (-1);
-	done = 0;
-	va_start(arg, format);
-	ft_memset(&spec, 0, sizeof(spec));
-	done = ft_vprintf(format, &arg, &spec, &done);
-	va_end(arg);
-	return (done);
+	else
+	{
+		(spec->buf)[spec->size] = c;
+		++spec->size;
+		if (spec->size == BUFF_SIZE)
+		{
+			write(STDOUT_FILENO, &spec->buf, spec->size);
+			spec->size = 0;
+		}
+	}
 }
